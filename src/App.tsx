502aa9bd6fdc39c324760cdb09ca64c8792d6baa
yuvalbl <yuval.bar.levi@gmail.com>
2019-09-10 23:17:35 +0300

    random console

--- src/App.tsx
+++ src/App.tsx
@@ -89,5 +89,15 @@ const App: React.FC = () => {
 
 export default App;
 
+const msgs = [
+  'הלו הלו, זה פרטי, למה להציץ?!',
+  'זהירות כלב נושך',
+  'למכירה: פומרניאן יד 2 במצב טוב',
+  'טרילילי טרה לה לה טרילילי',
+];
+
+const index = Math.floor(Math.random() * msgs.length);
+
+console.log(`%c${msgs[index]}`, 'color: red; font-size: 60px; font-weight: bold;');
 console.log('%cפשוט תגידו שאתם רוצים לראות את הקוד...', 'color: red; font-size: 30px; font-weight: bold;');
 console.log('https://github.com/yuvalbl/knesset-runner');
